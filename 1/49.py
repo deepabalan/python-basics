75db966ac45b21804cfaf64a8d1895ffffb2662c
deepabalan <deepa.deepabalan@gmail.com>
2017-04-27 00:40:50 +0530

    added 49 and 50

---
+++ 1/49.py
@@ -0,0 +1,12 @@
+
+
+word = 'Pineapple'
+
+if word < 'banana':
+    print 'Your word, ' + word + ', comes before banana.'
+
+elif word > 'banana':
+    print 'Your word, ' + word + ', comes after banana.'
+
+else:
+    print 'All right, bananas.'
